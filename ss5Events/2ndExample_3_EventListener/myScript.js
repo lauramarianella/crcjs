35b44971471949dd257cb3ae6857b1b7c5bbb53e
lauramarianella <lauramarianella@gmail.com>
2021-02-24 17:28:09 -0500

    Events listener

--- ss5Events/2ndExample_3_EventListener/myScript.js
+++
@@ -1,7 +0,0 @@
-
-//<script src="myScript.js"></script>
-(function(){
-    let btn = document.getElementById('idBtn');
-    btn.addEventListener('click', function(){ alert('you clicked me');}, false);
-    btn.addEventListener('click', function(){ alert('2nd function was call');}, false);
-})();
